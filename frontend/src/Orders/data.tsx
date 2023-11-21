67db4db5c7f05bb060944031b436c62cb7107ddb
Michael Edgington <edgington.63@osu.edu>
2023-11-21 14:28:06 -0500

    lab 5

---
+++ frontend/src/Orders/data.tsx
@@ -0,0 +1,39 @@
+export default {
+    orders: [{
+        _id:1,
+        createdAt:'09/29/2023',
+        totalPrice:199.25,
+        user: {
+            name:'Tim'
+        },
+        isPaid:true,
+        paidAt:'09/25/2023',
+        isDelivered: false,
+        deliveredDate: null,
+    },
+    {
+        _id:2,
+        createdAt:'10/05/2023',
+        totalPrice:132.66,
+        user: {
+            name:'Jennifer'
+        },
+        isPaid:true,
+        paidAt:'09/29/2023',
+        isDelivered: false,
+        deliveredDate: null,
+    },
+    {
+        _id:3,
+        createdAt:'09/29/2023',
+        totalPrice:302.97,
+        user: {
+            name:'Andrew'
+        },
+        isPaid:true,
+        paidAt:'09/25/2023',
+        isDelivered: false,
+        deliveredDate: null,
+    },
+]
+}
